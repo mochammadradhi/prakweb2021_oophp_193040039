1f83620acbafe6eac9acbe417af3256e7c8e2907
mochammadradhi <m.radhiakbar@gmail.com>
2021-09-23 11:31:47 +0700

    Menyelesaikan Tugas 2 OOPHP Visibility Video 4

---
+++ visibility.php
@@ -0,0 +1,88 @@
+<?php 
+class Produk{
+ public $judul,
+        $penulis,
+        $penerbit;
+
+ private $harga;
+
+ protected $diskon = 0;
+
+public function __construct($judul = "judul",$penulis = "penulis",$penerbit =  "penerbit",$harga = 0){
+    $this->judul = $judul;    
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+   
+}
+
+
+ public function getHarga(){
+    return $this->harga - ( $this->harga * $this->diskon/100);
+ }
+ 
+ public function getLabel(){
+     return "$this->penulis , $this->penerbit";
+ }
+
+ public function getInfoProduk(){
+    $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga}";
+    return $str;
+ }
+ 
+}
+
+class Komik extends Produk{
+    public $jmlh_halaman;
+
+    public function __construct($judul = "judul",$penulis = "penulis",$penerbit =  "penerbit",$harga = 0,$jmlh_halaman = 0){
+        parent::__construct($judul,$penulis,$penerbit,$harga);
+        $this->jmlh_halaman = $jmlh_halaman;
+    }
+
+    public function getInfoProduk(){
+        $str = "Komik : ". parent::getInfoProduk() .") - {$this->jmlh_halaman} Halaman.";
+     return $str;
+    }
+ }
+
+ class Game extends Produk{
+    public $waktu_main;
+
+    public function __construct($judul = "judul",$penulis = "penulis",$penerbit =  "penerbit",$harga = 0,$waktu_main = 0){
+        parent::__construct($judul,$penulis,$penerbit,$harga);
+        $this->waktu_main = $waktu_main;
+    }
+
+    public function getInfoProduk(){
+        $str = "Game : ". parent::getInfoProduk() .") ~ {$this->waktu_main} Jam.";
+     return $str;
+    }
+
+    public function setDiskon($diskon){
+        $this->diskon = $diskon;
+     }
+    
+ }
+
+class CetakInfoProduk{
+    public function cetak(Produk $produk){
+      $str =  "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga}) ";
+      return $str;
+    }
+}
+
+
+$produk1 = new Komik("Lord Of The Rings","J.R.R Tolkien","Allen & Unwin",30000, 100,"Komik");
+
+$produk2 = new Game("Star Wars","George Lucas","Lucasfilm Ltd",30000,50,"Game");
+
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();    
+echo "<hr>";
+
+$produk2->setDiskon(50);
+echo $produk2->getHarga();
+?>
